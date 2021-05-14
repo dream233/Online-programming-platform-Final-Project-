da962a6546e86b2ba66ce168db35a9b3a954300c
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-14 18:11:37 +0800

    add hello-world/src/App.vue.

---
+++ hello-world/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+	  <title1></title1>
+	  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+	import title1 from './components/title1.vue'
+	import Vue from 'vue'
+	export default{
+		data(){
+			return{
+				name:String
+			}
+		},
+		methods:{
+			
+		},
+		components:{
+			title1
+		}
+	}
+</script>
+
+<style>
+	
+</style>
