f2067a812cf84aef2b46d0f28678a92641bd145d
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-14 18:12:21 +0800

    update hello-world/src/main.js.

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,24 +1,14 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-//注意，本页面除了解除注释以外，请不要修改
-
-//通过切换不同的import实现界面的切换（因为跳转还没有写好）
-// 登陆界面，解除注释编译后就可以用
-// import App from './components/App_load.vue'   
-
-//主界面，用法同上
-import App from './components/App_main.vue'
-
+import App from './App.vue';
+import router from './router.js'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 
-new Vue
-(
-  {
-    render: h => h(App),
-  }
-).$mount('#app')
+new Vue({
+  router:router,
+  render: h => h(App),
+}).$mount('#app')
