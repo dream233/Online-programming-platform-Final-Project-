1dc6388b9d183de240f3344626020f6863d5695d
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-15 17:19:22 +0800

    update hello-world/src/conmponents/title1.vue. 优化界面

--- hello-world/src/conmponents/title1.vue
+++ hello-world/src/conmponents/title1.vue
@@ -10,19 +10,9 @@
 		  active-text-color="#ffd04b">
 		  <el-menu-item index="1" @click="moveto('/login');notshow()">登录</el-menu-item>
 		  <el-menu-item index="2" disabled>查看联系人</el-menu-item>
-		  <el-menu-item index="3" disabled>参与笔试</el-menu-item>
+		  <el-menu-item index="3" disabled>笔试平台</el-menu-item>
 		  <el-menu-item index="4" @click="moveto('/aboutus');notshow()">关于我们</el-menu-item>
 		</el-menu>
-		<div v-show="containerShow">
-			<el-container>
-			  <el-aside width="200px">这里是线上笔试平台</el-aside>
-			  <el-container>
-			    <el-header>上线时间：xxxxxx</el-header>
-			    <el-main>这里写一些介绍</el-main>
-			    <el-footer>联系方式：13535549796</el-footer>
-			  </el-container>
-			</el-container>
-		</div>
 	</div>
 </template>
 
@@ -41,46 +31,11 @@
 	      },
 		  moveto(path){
 			  this.$router.push(path);
-		  },
-		  notshow(){
-			  this.containerShow=0;
 		  }
 	    }
 	  }
 </script>
 
 <style>
-	.el-header, .el-footer {
-	    background-color: #B3C0D1;
-	    color: #333;
-	    text-align: center;
-	    line-height: 60px;
-	  }
-	  
-	  .el-aside {
-	    background-color: #D3DCE6;
-	    color: #333;
-	    text-align: center;
-	    line-height: 200px;
-	  }
-	  
-	  .el-main {
-	    background-color: #E9EEF3;
-	    color: #333;
-	    text-align: center;
-	    line-height: 160px;
-	  }
-	  
-	  body > .el-container {
-	    margin-bottom: 40px;
-	  }
-	  
-	  .el-container:nth-child(5) .el-aside,
-	  .el-container:nth-child(6) .el-aside {
-	    line-height: 260px;
-	  }
-	  
-	  .el-container:nth-child(7) .el-aside {
-	    line-height: 320px;
-	  }
+	
 </style>
