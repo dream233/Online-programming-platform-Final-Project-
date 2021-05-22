02e6f630689656eb988a1c53e6ec9c4a913f39ca
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-22 17:24:30 +0800

    update hello-world/src/components/title1.vue.

--- hello-world/src/components/title1.vue
+++ hello-world/src/components/title1.vue
@@ -8,10 +8,10 @@
 		  background-color="#545c64"
 		  text-color="#fff"
 		  active-text-color="#ffd04b">
-		  <el-menu-item index="1" @click="moveto('/login');notshow()">登录</el-menu-item>
-		  <el-menu-item index="2" disabled>查看联系人</el-menu-item>
-		  <el-menu-item index="3" disabled>笔试平台</el-menu-item>
-		  <el-menu-item index="4" @click="moveto('/aboutus');notshow()">关于我们</el-menu-item>
+		  <el-menu-item index="1" @click="moveto('/loginSuccess')">首页</el-menu-item>
+		  <el-menu-item index="2" @click="moveto('/loginSuccess/main')">联系人</el-menu-item>
+		  <el-menu-item index="3" @click="moveto('/loginSuccess/pblist')">面试题汇总</el-menu-item>
+		  <el-menu-item index="4" @click="moveto('/loginSuccess/aboutus')">关于我们</el-menu-item>
 		</el-menu>
 	</div>
 </template>
