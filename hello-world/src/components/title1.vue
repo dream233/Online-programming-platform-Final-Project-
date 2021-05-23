b8c6b53afe2c26d3a3a71e862e531de7b372f4fd
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-24 00:24:07 +0800

    更新一下

---
+++ hello-world/src/components/title1.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<el-menu
+		  :default-active="activeIndex2"
+		  class="el-menu-demo"
+		  mode="horizontal"
+		  @select="handleSelect"
+		  background-color="#545c64"
+		  text-color="#fff"
+		  active-text-color="#ffd04b">
+		  <el-menu-item index="1" @click="moveto('/loginSuccess')">首页</el-menu-item>
+		  <el-menu-item index="2" @click="moveto('/loginSuccess/main')">联系人</el-menu-item>
+		  <el-menu-item index="3" @click="moveto('/loginSuccess/pblist')">面试题汇总</el-menu-item>
+		  <el-menu-item index="4" @click="moveto('/loginSuccess/aboutus')">关于我们</el-menu-item>
+		</el-menu>
+	</div>
+</template>
+
+<script>
+	export default {
+	    data() {
+	      return {
+	        activeIndex: '1',
+	        activeIndex2: '1',
+			containerShow: '1'
+	      };
+	    },
+	    methods: {
+	      handleSelect(key, keyPath) {
+	        console.log(key, keyPath);
+	      },
+		  moveto(path){
+			  this.$router.push(path);
+		  }
+	    }
+	  }
+</script>
+
+<style>
+	
+</style>
---
+++ hello-world/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router.js'
+
+import VueCodemirror from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.use(VueCodemirror);
+
+Vue.use(VueQuillEditor, );
+
+new Vue({
+  router:router,
+  render: h => h(App),
+}).$mount('#app')
