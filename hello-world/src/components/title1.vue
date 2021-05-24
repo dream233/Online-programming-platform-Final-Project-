1a298be624f4cc6b092d8a78e1f2af264caff658
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-24 14:02:56 +0800

    添加了部分路由属性传递

---
+++ hello-world/src/components/title1.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<el-menu
+		  :default-active="activeIndex2"
+		  class="el-menu-demo"
+		  mode="horizontal"
+		  @select="handleSelect"
+		  background-color="#545c64"
+		  text-color="#fff"
+		  active-text-color="#ffd04b">
+		  <el-menu-item index="1" @click="moveto('/loginSuccess')">首页</el-menu-item>
+		  <el-menu-item index="2" @click="moveto('/loginSuccess/main')">联系人</el-menu-item>
+		  <el-submenu index="3">
+			  <template slot="title">
+				  面试题
+			  </template>
+			  <el-menu-item index="3-1" @click="moveto('/loginSuccess/pblist')">面试题汇总</el-menu-item>
+			  <el-menu-item index="3-2" @click="newProblem()">新建面试题</el-menu-item>
+		  </el-submenu>
+		  <el-menu-item index="4" @click="moveto('/loginSuccess/aboutus')">关于我们</el-menu-item>
+		</el-menu>
+	</div>
+</template>
+
+<script>
+	export default {
+	    data() {
+	      return {
+	        activeIndex: '1',
+	        activeIndex2: '1',
+			containerShow: '1'
+	      };
+	    },
+	    methods: {
+	      handleSelect(key, keyPath) {
+	        // console.log(key, keyPath);
+	      },
+		  moveto(path){
+			  this.$router.push(path);
+		  },
+		  newProblem(){
+			  //这里if条件判断当前用户的id
+			  if(0){
+				   this.$message({
+				            showClose: true,
+				            message: '只有面试官有权限',
+				            type: 'warning',
+							// center: true
+				          });
+			  }else{
+				  this.moveto('/loginSuccess/problemCreate');
+			  }
+		  }
+	    }
+	  }
+</script>
+
+<style>
+	
+</style>
