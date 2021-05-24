d2e6c1934e557b084c895b20f727497c0802a926
shawntam <9057796+shawntam@user.noreply.gitee.com>
2021-05-24 11:17:12 +0800

    修复了重复点击路由的报错，修改了注册界面

---
+++ hello-world/src/view/loginSuccess.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<title1></title1>
+		<router-view></router-view>
+	</div>
+	
+</template>
+
+<script>
+	import title1 from '../components/title1.vue'
+	export default {
+	    
+		components: {
+			title1
+		}
+	  }
+</script>
+
+<style>
+</style>
