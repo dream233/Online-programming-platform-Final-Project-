74c2cbafe5be59b3c58567b4c68d18215dd0e098
袁志琛 <8166300+yuan-zhichen@user.noreply.gitee.com>
2021-05-25 20:48:20 +0800

    面试题部分全部与后端连接

---
+++ hello-world/src/components/pblist.vue
@@ -0,0 +1,162 @@
+<template>
+	<div>
+		<el-table
+		    :data="tableData"
+		    stripe
+		    style="width: 100%">
+		    <el-table-column
+		      prop="id"
+		      label="题目id"
+		      width="1000">
+		    </el-table-column>
+			<el-table-column
+			  label="查看"
+			  width="120">
+			  <template slot-scope="scope" >
+				  <el-button type="primary" icon="el-icon-search"  @click="open1(scope.row.id)"></el-button>
+			  </template>
+			</el-table-column>
+			<el-table-column
+			  label="编辑"
+			  width="120">
+			  <template slot-scope="scope" >
+				  <el-button type="primary" icon="el-icon-edit" @click="open2(scope.row.id)"></el-button>
+			  </template>
+			</el-table-column>
+		  </el-table>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+	export default {
+	    data() {
+	      return {
+	        tableData: [],
+			problempassword:'',
+	      }
+	    },
+		methods: {
+			moveto(path){
+				var information = this.$route.query.information;
+				this.$router.push({
+								  path: path,
+								  query: {
+									  information
+								  }
+				});
+				
+			},
+			pass(){
+				return true
+			},
+			open1(id){/////////////////////////////////////////////////////////////////////////////////////////////
+				const path = 'http://127.0.0.1:5000/problemCheck';
+				console.log(id);
+				var pp = '';
+				var tem = {id:id};
+				axios.post(path,tem)
+					.then((res)=>{
+						//console.log(res.data.password);
+						pp=res.data.password;
+						console.log(pp);
+					});
+					
+				 this.$prompt('请输入密码', '提示', {
+				           confirmButtonText: '确定',
+				           cancelButtonText: '取消',
+				           // inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+						   inputValidator(value){
+									if(value==pp) return true;
+									else return false;
+						   },
+				           inputErrorMessage: '密码错误'
+				         }).then(({ value }) => {
+				           this.$message({
+				             type: 'success',
+				             message: '题目id为' + id
+				           });
+						   this.moveto('/loginSuccess/seeProblem/'+id+'/'+value);
+				         }).catch(() => {
+				           this.$message({
+				             type: 'info',
+				             message: '取消输入'
+				           });       
+				         });
+			},
+			open2(id){
+				 var pg = '';
+				 var pp = '';
+				 var information = this.$route.query.information;
+				 information = JSON.parse(information);
+				 if(information.id=='candidate'){
+				 				   this.$message({
+				 				            showClose: true,
+				 				            message: '只有面试官有权限',
+				 				            type: 'warning',
+				 							// center: true
+				 				          });
+				 }
+				 else{
+					 const path = 'http://127.0.0.1:5000/problemCheck';
+					 var tem = {id:id};
+					 axios.post(path,tem)
+					 	.then((res)=>{
+							pp=res.data.password;
+					 		pg=res.data.owner;
+							if(pg!=information.name){
+								 this.$message({
+										  showClose: true,
+										  message: '您没有编辑该题的权限',
+										  type: 'warning',
+										});
+							}
+							else{
+								 this.$prompt('请输入密码', '提示', {
+									 
+										   confirmButtonText: '确定',
+										   cancelButtonText: '取消',
+										   // inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+														   inputValidator(value){
+															//这里跟上面判断题目id和密码一样
+															   if(value==pp) return true;
+															   else return false;
+														   },
+										   inputErrorMessage: '密码错误'
+										 }).then(({ value }) => {
+										   this.$message({
+											 type: 'success',
+											 message: '题目id为' + id
+										   });
+		
+											this.moveto('/loginSuccess/problemEditor/'+id+'/'+value);
+										 }).catch(() => {
+										   this.$message({
+											 type: 'info',
+											 message: '取消输入'
+										   });       
+										 });
+							}
+					 	});
+					 //这里判断该题目是不是这个面试官创建的
+					 //用information.name表示用户邮箱
+					 //id表示题目id，判断是否符合
+					 //如果不符合，返回true
+					
+				 }
+			}
+		},
+		created(){
+				const path = 'http://127.0.0.1:5000/pblist';
+				axios.get(path,this.information)
+					.then((res)=>{
+						this.tableData = res.data.pblist;
+					})
+		}
+
+
+	  }
+</script>
+
+<style>
+</style>
---
+++ hello-world/src/components/problemCreate.vue
@@ -0,0 +1,161 @@
+<template>
+	<div>
+			<div class="edit_container">
+			        <quill-editor 
+			            v-model="content"
+			            ref="myQuillEditor" 
+			            :options="editorOption" 
+			            @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
+			            @change="onEditorChange($event)">
+			        </quill-editor>
+					<el-button type="primary" @click="dialogFormVisible = true">创建<i class="el-icon-upload el-icon--right"></i></el-button>
+					
+					<el-dialog title="题目信息(用于创建题目权限)" :visible.sync="dialogFormVisible">
+					  <el-form :model="information">
+					    <el-form-item label="题目id" :label-width="formLabelWidth">
+					      <el-input type="text" placeholder="请输入8位数字id" 
+						  v-model="pinformation.id" show-word-limit autocomplete="off" maxlength="8"></el-input>
+					    </el-form-item>
+					    <el-form-item label="题目密码" :label-width="formLabelWidth">
+					      <el-input v-model="pinformation.password" autocomplete="off"></el-input>
+					    </el-form-item>
+					  </el-form>
+					  <div slot="footer" class="dialog-footer">
+					    <el-button @click="dialogFormVisible = false">取 消</el-button>
+					    <el-button type="primary" @click="dialogFormVisible = false;saveHtml()">确 定</el-button>
+					  </div>
+					</el-dialog>
+			    </div>  
+		</div>	
+</template>
+
+<script>
+	// import hljs from 'highlight.js'
+	  import VueQuillEditor, { Quill } from 'vue-quill-editor'
+	  // import { ImageDrop } from 'quill-image-drop-module'
+	  import ImageResize from 'quill-image-resize-module'
+	  import axios from 'axios';
+	  // Quill.register('modules/imageDrop', ImageDrop)
+	  Quill.register('modules/imageResize', ImageResize)
+	export default {
+	  name: 'App',
+	  data(){
+	     return {
+	                 content: ``,
+	                 editorOption: {
+	                   modules:{
+	                               toolbar:[
+	                                 ['bold', 'italic', 'underline', 'strike'],    //加粗，斜体，下划线，删除线
+	                                 ['blockquote', 'code-block'],     //引用，代码块
+	                     
+	                     
+	                                 [{ 'header': 1 }, { 'header': 2 }],        // 标题，键值对的形式；1、2表示字体大小
+	                                 [{ 'list': 'ordered'}, { 'list': 'bullet' }],     //列表
+	                                 [{ 'script': 'sub'}, { 'script': 'super' }],   // 上下标
+	                                 [{ 'indent': '-1'}, { 'indent': '+1' }],     // 缩进
+	                                 [{ 'direction': 'rtl' }],             // 文本方向
+	                     
+	                     
+	                                 [{ 'size': ['small', false, 'large', 'huge'] }], // 字体大小
+	                                 [{ 'header': [1, 2, 3, 4, 5, 6, false] }],     //几级标题
+	                     
+	                     
+	                                 [{ 'color': [] }, { 'background': [] }],     // 字体颜色，字体背景颜色
+	                                 [{ 'font': [] }],     //字体
+	                                 [{ 'align': [] }],    //对齐方式
+	                     
+	                     
+	                                 ['clean'],    //清除字体样式
+	                                 ['image']    //上传图片
+	                     
+	                               ],
+								   imageResize: {
+									   displayStyles: {
+									                   backgroundColor: 'black',
+									                   border: 'none',
+									                   color: 'white'
+									                 },
+									                 modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
+								   }
+	                             },
+	                             //theme:'snow'
+	                           },
+							   dialogFormVisible: false,
+							           pinformation: {
+							             id: '',
+										 password: '',
+							           },
+							           formLabelWidth: '120px'
+	                 };
+	             },
+		computed: {
+	        editor() {
+	            return this.$refs.myQuillEditor.quill;
+	        },
+	    },
+		methods: {
+	        onEditorReady(editor) { // 准备编辑器
+	        },
+	        onEditorBlur(){}, // 失去焦点事件
+	        onEditorFocus(){}, // 获得焦点事件
+	        onEditorChange(){}, // 内容改变事件
+			saveHtml(){
+				var information = this.$route.query.information;
+				information = JSON.parse(information);
+				const path = 'http://127.0.0.1:5000/problemCreate';
+				var probleminfor = {id:this.pinformation.id,password:this.pinformation.password,owner:information.name,contents:this.content};
+				axios.post(path,probleminfor)
+					.then((res)=>{
+						if(res.data.message=='Y'){
+							this.$message({
+							          showClose: true,
+							          message: '题目创建成功',
+							          type: 'success'
+							        });
+						}
+						else{
+							this.$message({
+							          showClose: true,
+							          message: '题目id已存在',
+							          type: 'warning'
+							        });
+						}
+					})
+			  //这里判断题目是否已存在
+			  //this.pinformation.id表示题目id
+			  //this.pinformation.password表示题目密码
+			  //如果不是已存在，那么返回true，替换下面的判断条件
+			  /*const path = 'http://127.0.0.1:5000/problemCreate';
+			  axios.post(path,this.pinformation.id)
+					.then((res)=>{
+						
+					})*/
+			  /* if(!(this.pinformation.id=='1')){
+				  this.$message({
+				            showClose: true,
+				            message: '题目创建成功',
+				            type: 'success'
+				          });
+				  //这里把this.content存入对应的题目id里，this.content为内容
+				  //
+				  
+			  }
+			  else {
+				  this.$message({
+				            showClose: true,
+				            message: '题目id已存在',
+				            type: 'warning'
+				          });
+			  } */
+			}
+	    },
+		created(){
+			window.that=this;
+			
+		}
+	}
+</script>
+
+<style>
+	
+</style>
